15273520985b5ccd0ee85e7a6b08978ff5be8885
YepJohnWayne <efyjy8@nottingham.edu.my>
2023-02-23 13:55:20 +0800

    Tested some card layouts

--- question.php
+++ question.php
@@ -1,5 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
-    <link rel="stylesheet" href="style.css">
-    <body class="background2"></body>
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Comfortaa" />
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+<body class="background2">
+    <br>
+    <h1 class="heading2">
+        Questionnaire
+    </h1>
+    <br>
+    <br>
+    <div class="card">
+        <div class="card-body">
+            <p class="paragraph">
+                Questions
+            </p>
+        </div>
+    </div>
+
+
+
+
+
+</body>
+
 </html>
