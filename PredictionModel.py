f5927de6cf606f30910f7176e41b1110ec8bbed8
chaser6061 <japhialoo@gmail.com>
2023-03-20 18:17:44 +0800

    Added Comments explaining code.

--- PredictionModel.py
+++ PredictionModel.py
@@ -3,13 +3,14 @@ import joblib
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import AdaBoostClassifier
 import pandas as pd
-# Script to create prediction model and save locally as a joblib file for external use
+# Script to create prediction model and save locally as a joblib file for external use 
+# Not required for final website
 
-# Loading training data set into an array.
+# Loading training data set and expected output into an array.
 s = np.array(pd.read_excel('OSA_Dataset301_2YrGp.xls',usecols='A:J', nrows=300, skiprows=1))
 VarNames = np.array(['Outcome''A''B''C''D''E''F''G''H''I'])
 
-# Separating data into training data, X, and predicted value, Y.
+# Separating data into training data, X, and expected output value, Y.
 X = np.array(s[:,1:], dtype=np.float64)
 Y = np.array(s[:,0], dtype=np.int64)
 
@@ -23,5 +24,6 @@ joblib.dump(Model, filename )
 
 # Test code for current prediction model to check accuracy
 # Testing Model Values [0,1,30,0,6,0,0,0,0] [3,2,41,1,10,1,1,0,0] [1,1,0,0,4,0,0,0,0]
+# Input array X in Model.Predict(X) is in 2D array format as the training model was trained in 2D array format ([Set1, Set2,...]) where Set1 = [a,b,c,d,e,f,g,h,i].
 predictOSA = Model.predict([[3,2,41,1,10,1,1,0,0]])
 print(predictOSA)
