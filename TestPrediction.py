711e68df09c1ebb348733552f44a43c1545c77ee
chaser6061 <japhialoo@gmail.com>
2023-03-22 20:37:38 +0800

    Edited negative result page and TestPrediction.py

--- TestPrediction.py
+++ TestPrediction.py
@@ -1,15 +1,6 @@
 import joblib 
 # Script to test saved model
 load_model = joblib.load('CompleteModel.joblib')
-a = input()
-b = input()
-c = input()
-d = input()
-e = input()
-f = input()
-g = input()
-h = input()
-i = input()
-values = [[a,b,c,d,e,f,g,h,i]]
+values = [[int(x) for x in input().split()]]
 result = load_model.predict(values)
 print(result)
