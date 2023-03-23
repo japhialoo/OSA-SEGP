f143ec8be93f51598989056e44d3c0dd387ff36a
YepJohnWayne <efyjy8@nottingham.edu.my>
2023-03-23 22:34:20 +0800

    modified to accept command line

--- TestPrediction.py
+++ TestPrediction.py
@@ -1,4 +1,5 @@
 import joblib 
+import sys
 # Script to test saved model
 # the TestPrediction.spec and TestPrediction files are my attempt in converting pythong files to executable .exe files
 # it didn't work when i tried to execute it so i'll delete it later.
@@ -6,6 +7,6 @@ import joblib
 # when the python script is called from php it somehow gets stuck and does not reach the print result line.
 
 load_model = joblib.load('CompleteModel.joblib')
-values = [[int(x) for x in input().split()]]
+values = [[int(x) for x in sys.argv[1].split()]]
 result = load_model.predict(values)
 print(result)
--- testPythong.php
+++ testPythong.php
@@ -1,10 +1,7 @@
 <?php
-# Currently does not work but here is the code that i used to try executing a python script with input values.
-$args = '3 2 41 1 10 1 1 0 0';
-
-#$command = "python3 TestPrediction.py";
-
-$output = shell_exec("python TestPrediction.py $args");
+$input_values = '"3 2 41 1 10 1 1 0 0"'; // added ""
+$command = 'python TestPrediction.py ' . escapeshellarg($input_values); //python instead of python3 worked for my machine
+$output = shell_exec($command);
 
 if ($output == null){
     echo "Output is null";
