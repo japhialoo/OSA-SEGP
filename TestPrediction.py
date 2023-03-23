ef8b630145362232772e98d82169fb1b5f7fcee4
chaser6061 <japhialoo@gmail.com>
2023-03-23 13:30:38 +0800

    Added comments

--- TestPrediction.py
+++ TestPrediction.py
@@ -1,6 +1,10 @@
 import joblib 
 # Script to test saved model
+# the TestPrediction.spec and TestPrediction files are my attempt in converting pythong files to executable .exe files
+# it didn't work when i tried to execute it so i'll delete it later.
+# if you want to know how i did it you can search up pyinstaller and see how hey convert .py to .exe from your cmd prompt/terminal
 # when the python script is called from php it somehow gets stuck and does not reach the print result line.
+
 load_model = joblib.load('CompleteModel.joblib')
 values = [[int(x) for x in input().split()]]
 result = load_model.predict(values)
