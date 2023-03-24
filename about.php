62d2943e6665df84ab2ad122ac5264f00f416d67
Jad <jad.samaam15@gmail.com>
2023-03-24 14:17:21 +0800

    commit pages

--- about.php
+++ about.php
@@ -8,7 +8,7 @@
     <script type="text/javascript" src="script.js"></script>
 </head>
 
-<body>
+<body class="background3 bodytext">
     <nav class="navbar navbar-expand-lg bg-body-tertiary position-sticky">
         <div class="container-fluid">
             <a class="navbarLinks" href="MainPage.php">Home</a>
@@ -24,15 +24,48 @@
             </div>
         </div>
     </nav>
-    <center>
-        <div class="faq">
-            <img class="faq" src="backgrounds/faq.svg">
-        </div>
-        <div class="gifstyle">
-            <img class="gifstyle" src="Media/car_crash.gif">
-        </div>
-    </center>
+    <h1 class="h1" style="text-align:center; color:blue; font-family:Comfortaa">Learn More.</h1>
     <br>
+    <p class="paragraph3" style="text-align:left; font-family:Comfortaa">
+    <h3 >
+        <b>Dangers of Obstructive Sleep Apnea.</b>
+    </h3>
+    <br>
+    <h5>
+        <b>Daytime Sleepiness.</b>
+    </h5>
+    Drivers that have to travel long distances are more prone to daytime sleepiness.
+    <br>
+    This is very dangerous especially if the patient is showing signs of OSA.
+    <br>
+    Daytime sleepiness is one of the major causes of accidents for people diagnosed with OSA.
+    <br>
+    <br>
+    <br>
+    <br>
+    <h3>
+        <b>How our questionnaire works.</b>
+    </h3>
+    <br>
+        The questionnaire once answered will send your answers to our prediction model, where your answers are taken as inputs and produces
+        a result of negative or positive.
+        <br>
+        Our prediction model was trained with real data from patients and produces the most possible accurate result it can according to your inputs.
+        <br>
+        The database the model was trained on has the data from over 100 patients polysomnography test.
+    </p>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <h4 style="color:red">
+        Disclaimer: <h6>
+            This prediction is not 100%, it is just a mere prediction. It is a way to save you time and money, but we still recommend getting your result
+            verified by a medical professional.
+        </h6>
+    </h4>
     <br>
     <br>
     <br>
