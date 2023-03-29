eef761502a0cf87e1c4c0cc7de3aad3a8795d931
YepJohnWayne <efyjy8@nottingham.edu.my>
2023-03-29 14:16:36 +0800

    Test Loader

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
-document.getElementById("myForm").addEventListener("submit", function(event) {
-    // Get the weight and height input values
-    var weight = parseFloat(document.getElementById("W").value);
-    var height = parseFloat(document.getElementById("H").value);
-    var NC = parseFloat(document.getElementById("NC").value);
+document.getElementById("myForm").addEventListener("submit", function (event) {
+  // Get the weight and height input values
+  var weight = parseFloat(document.getElementById("W").value);
+  var height = parseFloat(document.getElementById("H").value);
+  var NC = parseFloat(document.getElementById("NC").value);
+
+  // Check if weight and height are greater than 0
+  if (weight <= 0) {
+    // Prevent the form from being submitted
+    event.preventDefault();
+
+    // Show an error message
+    alert("Weight must be greater than 0");
+  }
+
+  if (height <= 0) {
+
+    event.preventDefault();
+
+    alert("Height must be greater than 0");
+  }
+
+  if (NC <= 0) {
+
+    event.preventDefault();
+
+    alert("Neck Circumference must be greater than 0");
+  }
+});
+
+const submitButton = document.querySelector('button[type="submit"]');
+submitButton.addEventListener('click', showLoadingPage);
+
+
+function showLoadingPage() {
   
-    // Check if weight and height are greater than 0
-    if (weight <= 0) {
-      // Prevent the form from being submitted
-      event.preventDefault();
-      
-      // Show an error message
-      alert("Weight must be greater than 0");
-    }
-
-    if (height <=0){
-
-        event.preventDefault();
-
-        alert("Height must be greater than 0");
-    }
-
-    if (NC <=0){
-
-        event.preventDefault();
-
-        alert("Neck Circumference must be greater than 0");
-    }
-  });
   
+}
+
+
