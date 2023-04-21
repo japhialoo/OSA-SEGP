1b0187ce686bae3fa59c91003ac8bf3fb6dcde66
chaser6061 <japhialoo@gmail.com>
2023-04-21 16:57:19 +0800

    Editted

--- ML/TestPrediction.py
+++ ML/TestPrediction.py
@@ -1,6 +1,7 @@
 import joblib 
 import sys
 
+# Used to test the exported Prediction Model
 load_model = joblib.load('PredictionModel.joblib')
 values = [[int(x) for x in sys.argv[1].split()]]
 result = load_model.predict(values)
